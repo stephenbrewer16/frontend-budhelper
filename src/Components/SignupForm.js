443a47d701de184d4c65d75b1cc555cc774057e1
Stephen Brewer <stephebrewer16@gmail.com>
2019-11-15 15:18:11 -0500

    functions for login and signup

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -3,9 +3,39 @@ import React, { Component } from 'react'
 export default class SignupForm extends Component {
 
     state = {
+        username: '',
+        password: '',
+        email: '',
+        wallet: null
 
     }
-    
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify({ username: this.state.username, password: this.state.password, email: this.state.email})
+        })
+            .then(resp => resp.json())
+            .then(response => {
+                if (response.errors) {
+                    alert(response.errors)
+                } else {
+                    this.props.setUser(response)
+                }
+            })
+        }
+
     render() {
         return (
             <div className="login-box">
