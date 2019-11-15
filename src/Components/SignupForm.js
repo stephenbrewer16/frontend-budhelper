60da8acb46cc6cf72c948bafc4f0db3105540223
Stephen Brewer <stephebrewer16@gmail.com>
2019-11-15 14:55:49 -0500

    adding forms to signup and login

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class SignupForm extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="login-box">
+                <form className="login-form">
+
+                </form>
             </div>
         )
     }
