5f6587f48309fd8ebc70f52eda7ecc07afb1dbbb
Stephen Brewer <stephebrewer16@gmail.com>
2019-11-15 15:21:27 -0500

    adding state to app with current user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import './App.css';
+import SignupForm from './components/SignupForm';
 
 export default class App extends Component {
+  state = {
+    currentUser = null
+  }
+  
   render() {
     return (
+
       <div>
         
       </div>
