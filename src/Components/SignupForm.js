fbb69297f2349602adc501e9789cd77719eff12a
Stephen Brewer <stephebrewer16@gmail.com>
2019-11-15 15:02:16 -0500

    adding forms

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 export default class SignupForm extends Component {
+
+    state = {
+
+    }
+    
     render() {
         return (
             <div className="login-box">
